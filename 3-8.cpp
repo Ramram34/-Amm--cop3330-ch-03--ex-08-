8ba28e330429f5410d8a6418097edf4f2f2155e8
Ramram34 <90494421+Ramram34@users.noreply.github.com>
2021-12-11 18:14:51 -0500

    Add files via upload

---
+++ 3-8.cpp
@@ -0,0 +1,21 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Rami Amm
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+
+  cout << "Enter an integer: ";
+  cin >> n;
+
+  if ( n % 2 == 0)
+    cout << n << " is even.";
+  else
+    cout << n << " is odd.";
+
+  return 0;
+}
